4c6647ffb48e4e5704f5306f8609eea754655f70
Randriharimalala <hei.finoana.8@gmail.com>
2024-07-26 21:55:11 +0300

    add ajouterPossession

--- models/Personne.js
+++ models/Personne.js
@@ -1,6 +1,10 @@
 class Personne {
   constructor(nom) {
     this.nom = nom;
+    this.patrimoine = new Patrimoine(nom, new Date(), []); // Initialisation du patrimoine
+  }
+  ajouterPossession(possession) {
+    this.patrimoine.addPossession(possession);
   }
 }
 
