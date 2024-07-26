417dbe3c9f9428ba8fb0bd413b39a4537384dfb3
Randriharimalala <hei.finoana.8@gmail.com>
2024-07-26 21:58:51 +0300

    feat: dateAcquisition, valeur

--- models/Possession.js
+++ models/Possession.js
@@ -1,8 +1,10 @@
 class Possession {
-  constructor(possesseur, type, libelle) {
+  constructor(possesseur, type, libelle,dateAcquisition, valeur) {
     this.possesseur = possesseur;
     this.type = type;
     this.libelle = libelle;
+    this.dateAcquisition = dateAcquisition;
+    this.valeur = valeur;
   }
 }
 module.exports = Possession;
