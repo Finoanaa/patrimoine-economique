b2f536f29c8931231fbb5f5a1ec3435856806d82
Randriharimalala <hei.finoana.8@gmail.com>
2024-07-26 21:52:31 +0300

    add calculerTrainDeVie

--- models/Patrimoine.js
+++ models/Patrimoine.js
@@ -1,20 +1,27 @@
 class Patrimoine {
   constructor(possesseur, date, possessions) {
-    this.possesseur  = possesseur;
+    this.possesseur = possesseur;
     this.date = date
     this.possessions = possessions; // [Possession, Possession, ...]
+    this.trainDeVie = 0; // Initialisation du train de vie à zéro
   }
   getValeur(date) {
-  // Calculer la valeur totale du patrimoine à la date spécifiée
-  let valeurTotale = 0;
-  for (const possession of this.possessions) {
-    // Supposons que chaque possession a une propriété "valeur" définie
-    if (possession.dateAcquisition <= date) {
-      valeurTotale += possession.valeur;
+    // Calculer la valeur totale du patrimoine à la date spécifiée
+    let valeurTotale = 0;
+    for (const possession of this.possessions) {
+      // Supposons que chaque possession a une propriété "valeur" définie
+      if (possession.dateAcquisition <= date) {
+        valeurTotale += possession.valeur;
+      }
     }
+    return valeurTotale;
   }
-  return valeurTotale;
-}
+  calculerTrainDeVie(date) {
+    const valeurActuelle = this.getValeur(date);
+    const valeurPrecedente = this.getValeur(date - 1); // Par exemple, le mois précédent
+    this.trainDeVie = valeurActuelle - valeurPrecedente;
+    return this.trainDeVie;
+  }
   addPossession(possession) {
     this.possessions.push(possession);
   }
