e3a7d842b0d5d0a34a0e7f95ecc4bd6178adb1db
Randriharimalala <hei.finoana.8@gmail.com>
2024-07-26 22:11:08 +0300

    feat: put a link between salaire and train de vie

--- models/Personne.js
+++ models/Personne.js
@@ -2,9 +2,14 @@ class Personne {
   constructor(nom) {
     this.nom = nom;
     this.patrimoine = new Patrimoine(nom, new Date(), []); // Initialisation du patrimoine
+    this.salaireMensuel = salaireMensuel;
   }
   ajouterPossession(possession) {
     this.patrimoine.addPossession(possession);
+  }
+  calculerTrainDeVieMensuel(depenses) {
+    const totalDepenses = depenses.reduce((acc, depense) => acc + depense, 0);
+    return this.salaireMensuel - totalDepenses;
   }
 }
 
